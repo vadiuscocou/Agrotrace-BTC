79f82be335b6f19a0f6e84a7d2ae402649bf4538
Léandre ELISHA <leandreelisha20@gmail.com>
2026-06-25 11:44:12 +0100

    Set lang to fr to prevent Google Translate from changing AgroTrace to Trace Agro

--- AgroTraceLaravel/resources/views/welcome.blade.php
+++ AgroTraceLaravel/resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" class="scroll-smooth">
+<html lang="fr" class="scroll-smooth">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
