240933c66c75e0609f17206ece7ef7c3a9d92554
Léandre ELISHA <leandreelisha20@gmail.com>
2026-07-04 11:34:43 +0100

    Clean up temporary scripts

--- AgroTraceLaravel/update_coords.php
+++
@@ -1,8 +0,0 @@
-<?php
-App\Models\Project::whereNull('latitude')->get()->each(function($p) {
-    $p->update([
-        'latitude' => round(6.5 + (mt_rand() / mt_getrandmax()) * 5.0, 6), 
-        'longitude' => round(1.5 + (mt_rand() / mt_getrandmax()) * 2.0, 6)
-    ]);
-});
-echo "OK\n";
